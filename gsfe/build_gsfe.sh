1e8e6140508dbff58a46335782a6ca8ecdbe16a7
Shuozhi Xu <shuozhixu@gmail.com>
2024-02-11 13:55:10 -0600

    build

--- gsfe/build_gsfe.sh
+++ gsfe/build_gsfe.sh
@@ -4,7 +4,7 @@ rm -f *.lmp data.*
 
 a=3.55
 
-atomsk --create fcc $a Al orient [1-10] [11-2] [111] -duplicate 20 20 20 Al.cfg
+atomsk --create fcc $a Al orient [1-10] [11-2] [111] -duplicate 30 30 10 Al.cfg
 
 atomsk Al.cfg -select random 11162 Al -sub Al Co AlCo.cfg
 
