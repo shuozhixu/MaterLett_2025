a4f852f2bc532ab3d38fe4aa42fc1eca10afa4bc
Shuozhi Xu <shuozhixu@gmail.com>
2024-04-29 18:15:27 -0500

    Update post_jobs.sh

--- gsfe/post_jobs.sh
+++ gsfe/post_jobs.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-rm -f gsfe_ext_surface test gamma.txt
+rm -f gsfe_ext_surface gamma_test gamma.txt
 
-for i in `seq 0 1 64`
+cd 0
+awk -v c=1 'NR==1 { b=$2 } { printf( "%0.5f\t%0.7f\n", $1,( $2-b ) * c ) }' gsfe_ori > gsfe
+cd ..
+
+for i in `seq 0 1 2`
 do
         a=0.015625
         a=`echo "scale=6;$a*$i"|bc`
         cd $i
-        rm -f gsfe gsfe_ext
-        awk -v c=1 'NR==1 { b=$2 } { printf( "%0.2f\t%0.6f\n", $1,( $2-b ) * c ) }' gsfe_ori > gsfe
-        gawk -v var="$a" '{print var, $1, $2}' gsfe > gsfe_ext
+        rm -f gsfe_ext
+        awk -v var="$a" '{print var, $1, $2}' gsfe_ori > gsfe_ext
         cat gsfe_ext >> ../gsfe_ext_surface
         cd ..
 done
 
-awk '{$3; print}' gsfe_ext_surface > test
-sort -nk2 -nk1 test > gamma.txt
+sort -nk2 -nk1 gsfe_ext_surface > gamma_test
+awk 'NR==1 { b=$3 } { printf( "%0.5f\t%0.5f\t%0.7f\n", $1, $2, $3-b) }' gamma_test > gamma.txt
 sed -i '1s/^/65 101\n/' gamma.txt
 
-rm -f gsfe_ext_surface test
+rm -f gsfe_ext_surface gamma_test
